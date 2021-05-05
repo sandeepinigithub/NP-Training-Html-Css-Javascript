4734f326ec088c21660956058d991cc791049fd8
Sandeep Shukla <sandeepini.2012@gmail.com>
2021-05-05 14:59:54 +0530

    cover every functionality done

--- task3-formValidation/script.js
+++ task3-formValidation/script.js
@@ -1,7 +1,8 @@
 let slides = document.getElementsByClassName("mySlides");
 let prev = document.querySelector(".prev");
 let next = document.querySelector(".next");
-let submit = document.querySelector(".submit")
+let submit = document.querySelector(".submit");
+let print = document.querySelector(".print");
 
 
 if (!slides.length == 0) {
@@ -43,11 +44,11 @@ if (!slides.length == 0) {
     }
 }
 function getValue(){
-    let name = document.getElementById('name').value;
-    let email = document.getElementById('email').value;
-    let date = document.getElementById('date').value;
-    let gender = document.querySelector('input[name="gender"]:checked').value
-    let maritalstatus = document.getElementById('maritalstatus').value;
+    // let name = document.getElementById('name').value;
+    // let email = document.getElementById('email').value;
+    // let date = document.getElementById('date').value;
+    // let gender = document.querySelector('input[name="gender"]:checked').value
+    // let maritalstatus = document.getElementById('maritalstatus').value;
 
     let rollno = document.getElementById('rollno').value;
     let course = document.getElementById('course').value;
@@ -62,14 +63,48 @@ function getValue(){
     let country = document.getElementById('country').value;
 
     let occupationtype = document.getElementById('occupationtype').value;
-    let occupationstatus = document.getElementById('occupationtstatus').value;
+    let occupationstatus = document.getElementById('occupationstatus').value;
     let employername = document.getElementById('employername').value;
     let startdate = document.getElementById('startdate').value;
-    let designation = document.getElementById('designation').value;
-
-    
+    let designation = document.getElementById('designation').value;    
     // console.log(name,email,date,gender,maritalstatus,rollno,course,branch,currentsemester,college,address,city,state,zipcode,country,occupationtype,occupationstatus,employername,startdate,designation);
-    
+
+    slides[3].style.display = "none";
+    prev.style.display = "none";
+    submit.style.display = "none";
+    setValue();
+    print.style.display = "initial";
+    document.getElementById('printContainer').style.display = "initial";
+
+}
+function setValue(){
+    document.getElementById('nameValue').innerHTML = document.getElementById('name').value;
+    document.getElementById('emailValue').innerHTML = document.getElementById('email').value;
+    document.getElementById('dateValue').innerHTML = document.getElementById('date').value;
+    // document.getElementById('genderValue').innerHTML = document.getElementById('gender').value;
+    // document.getElementById('maritalstatusValue').innerHTML = document.getElementById('maritalstatus').value;
+
+    document.getElementById('rollnoValue').innerHTML = document.getElementById('rollno').value;
+    document.getElementById('courseValue').innerHTML = document.getElementById('course').value;
+    document.getElementById('branchValue').innerHTML = document.getElementById('branch').value;
+    document.getElementById('currentsemesterValue').innerHTML = document.getElementById('currentsemester').value;
+    document.getElementById('collegeValue').innerHTML = document.getElementById('college').value;
+
+    document.getElementById('addressValue').innerHTML = document.getElementById('address').value;
+    document.getElementById('cityValue').innerHTML = document.getElementById('city').value;
+    document.getElementById('stateValue').innerHTML = document.getElementById('state').value;
+    document.getElementById('zipcodeValue').innerHTML = document.getElementById('zipcode').value;
+    document.getElementById('countryValue').innerHTML = document.getElementById('country').value;
+
+    document.getElementById('occupationtypeValue').innerHTML = document.getElementById('occupationtype').value;
+    document.getElementById('occupationstatusValue').innerHTML = document.getElementById('occupationstatus').value;
+    document.getElementById('employernameValue').innerHTML = document.getElementById('employername').value;
+    document.getElementById('startdateValue').innerHTML = document.getElementById('startdate').value;
+    document.getElementById('designationValue').innerHTML = document.getElementById('designation').value;
+
+}
+function printValue(){
+    window.print();    
 }
 
 prev.addEventListener("click", () => {
@@ -80,3 +115,4 @@ next.addEventListener("click", () => {
     plusSlides(1);
 });
 submit.addEventListener("click", getValue);
+print.addEventListener("click", printValue);
