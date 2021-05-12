04e66d44f7f43aefcf2d8d0528a10c007e38a318
Sandeep Shukla <sandeepini.2012@gmail.com>
2021-05-12 22:59:41 +0530

    check

--- task3-formValidation-Modified/javascript/script.js
+++ task3-formValidation-Modified/javascript/script.js
@@ -5,8 +5,6 @@ var prevBtn = document.querySelector('.prev');
 var submitBtn = document.querySelector('.submit');
 var printBtn = document.querySelector('.print');
 var errorSpan = document.querySelectorAll('.error')
-//input field
-var zipCode = document.getElementById('zipcode');
 
 document.querySelector('.step' + step).style.display = 'block';
 document.querySelector('.indicate' + step).classList.add('step-indicator-fill');
@@ -94,8 +92,14 @@ function setError(id, error) {
 function validate(inputID) {
     var input = document.getElementById(inputID);
     // step 1
-    if (input.value == "" && inputID == "name") {
-        setError("erName", "Please enter your name");
+    if (inputID == "name") {
+        if (input.value == "") {
+            setError("erName", "Please enter your name");
+        }
+        else {
+            setError("erName", "");
+        }
+
         isError = true;
     }
     else if (inputID == "email") {
@@ -103,89 +107,185 @@ function validate(inputID) {
         if (input.value == "") {
             setError("erEmail", "Please enter your email address");
         }
-        if (input.value != "" && !re.test(input.value.trim())) {
+        else if (input.value != "" && !re.test(input.value.trim())) {
             setError("erEmail", "Please enter valid email");
+        }
+        else {
+            setError("erEmail", "");
         }
 
         isError = true;
     }
-    else if (input.value == "" && inputID == "dob") {
-        setError("erDob", "Please enter your Date Of Birth");
+    else if (inputID == "dob") {
+        if (input.value == "") {
+            setError("erDob", "Please enter your Date of birth");
+        }
+        else {
+            setError("erDob", "");
+        }
+
         isError = true;
     }
 
     // step 2
-    else if (input.value == "" && inputID == "rollno") {
-        setError("erRollno", "Please enter your Roll Number");
+    else if (inputID == "rollno") {
+        if (input.value == "") {
+            setError("erRollno", "Please enter your Roll Number");
+        }
+        else {
+            setError("erRollno", "");
+        }
+
         isError = true;
     }
-    else if (input.value == "" && inputID == "course") {
-        setError("erCourse", "Please enter your Course");
+    else if (inputID == "course") {
+        if (input.value == "") {
+            setError("erCourse", "Please enter your Course");
+        }
+        else {
+            setError("erCourse", "");
+        }
+
         isError = true;
     }
-    else if (input.value == "" && inputID == "branch") {
-        setError("erBranch", "Please enter your Course Branch");
+    else if (inputID == "branch") {
+        if (input.value == "") {
+            setError("erBranch", "Please enter your Course Branch");
+        }
+        else {
+            setError("erBranch", "");
+        }
+
         isError = true;
     }
-    else if (input.value == "" && inputID == "currentsemester") {
-        setError("erCurrentsemester", "Please enter your Current Semester");
+    else if (inputID == "currentsemester") {
+        if (input.value == "") {
+            setError("erCurrentsemester", "Please enter your Current Semester");
+        }
+        else {
+            setError("erCurrentsemester", "");
+        }
+
         isError = true;
     }
-    else if (input.value == "" && inputID == "college") {
-        setError("erCollege", "Please enter your College Name");
+    else if (inputID == "college") {
+        if (input.value == "") {
+            setError("erCollege", "Please enter your College Name");
+        }
+        else {
+            setError("erCollege", "");
+        }
+
         isError = true;
     }
 
 
 
     // step 3
-    else if (input.value == "" && inputID == "address") {
-        setError("erAddress", "Please enter your Address");
+    else if (inputID == "address") {
+        if (input.value == "") {
+            setError("erAddress", "Please enter your Address");
+        }
+        else {
+            setError("erAddress", "");
+        }
+
         isError = true;
     }
-    else if (input.value == "" && inputID == "city") {
-        setError("erCity", "Please enter your City");
+    else if (inputID == "city") {
+        if (input.value == "") {
+            setError("erCity", "Please enter your City");
+        }
+        else {
+            setError("erCity", "");
+        }
+
         isError = true;
     }
-    else if (input.value == "" && inputID == "state") {
-        setError("erState", "Please enter your State");
+    else if (inputID == "state") {
+        if (input.value == "") {
+            setError("erState", "Please enter your State");
+        }
+        else {
+            setError("erState", "");
+        }
+
         isError = true;
     }
     else if (inputID == "zipcode") {
         if (input.value == "") {
             setError("erZipcode", "Please enter your Zipcode");
         }
-        if (input.value != "" && input.value.length < 6) {
+        else if (input.value != "" && input.value.length < 6) {
             setError("erZipcode", "Please enter zipcode of valid length ");
+        }
+        else {
+            setError("erZipcode", "");
         }
 
         isError = true;
     }
-    else if (input.value == "" && inputID == "country") {
-        setError("erCountry", "Please enter your Country");
+    else if (inputID == "country") {
+        if (input.value == "") {
+            setError("erCountry", "Please enter your Country");
+        }
+        else {
+            setError("erCountry", "");
+        }
+
         isError = true;
     }
 
 
     // Step 4
-    else if (input.value == "" && inputID == "occupationtype") {
-        setError("erOccupationtype", "Please enter your Occupationtype");
+    else if (inputID == "occupationtype") {
+        if (input.value == "") {
+            setError("erOccupationtype", "Please enter your Occupationtype");
+        }
+        else {
+            setError("erOccupationtype", "");
+        }
+
         isError = true;
     }
-    else if (input.value == "" && inputID == "occupationstatus") {
-        setError("erOccupationstatus", "Please enter your Occupationstatus");
+    else if (inputID == "occupationstatus") {
+        if (input.value == "") {
+            setError("erOccupationstatus", "Please enter your Occupationstatus");
+        }
+        else {
+            setError("erOccupationstatus", "");
+        }
+
         isError = true;
     }
-    else if (input.value == "" && inputID == "employername") {
-        setError("erEmployername", "Please enter your Employername");
+    else if (inputID == "employername") {
+        if (input.value == "") {
+            setError("erEmployername", "Please enter your Employername");
+        }
+        else {
+            setError("erEmployername", "");
+        }
+
         isError = true;
     }
-    else if (input.value == "" && inputID == "startdate") {
-        setError("erStartdate", "Please enter your startdate");
+    else if (inputID == "startdate") {
+        if (input.value == "") {
+            setError("erStartdate", "Please enter your startdate");
+        }
+        else {
+            setError("erStartdate", "");
+        }
+
         isError = true;
     }
-    else if (input.value == "" && inputID == "designation") {
-        setError("erDesignation", "Please enter your designation");
+    else if (inputID == "designation") {
+        if (input.value == "") {
+            setError("erDesignation", "Please enter your designation");
+        }
+        else {
+            setError("erDesignation", "");
+        }
+
         isError = true;
     }
 
