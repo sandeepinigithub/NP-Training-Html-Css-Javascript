88f2e0a36988a5d7fc01dc98bce5341d39a553fb
Sandeep Shukla <sandeepini.2012@gmail.com>
2021-05-17 00:30:08 +0530

    task4DynamicTable-final done

--- task4-dynamicTable/javascript/script.js
+++ task4-dynamicTable/javascript/script.js
@@ -1,5 +1,6 @@
 let table = document.getElementById("userTable");
 let temp = "";
+let temp2 = "";
 function getData() {
     const xhr = new XMLHttpRequest();
     xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
@@ -19,16 +20,33 @@ function getData() {
         document.getElementById("userData").innerHTML = temp;
         jsonData.forEach((element) => {
             var view = document.getElementById("view" + element.id);
-            view.addEventListener("click",function (){
-                viewData(element.id,element.name,element.username,element.email,element.phone,element.website)
+            view.addEventListener("click", function () {
+                viewData(element.id, element.name, element.username, element.email, element.phone, element.website)
             });
         });
     };
     xhr.send();
 }
 
-function viewData(u_id,u_name,u_username,u_email,u_phone,u_website) {
-    console.log(u_id,u_name,u_username,u_email,u_phone,u_website);
+function viewData(u_id, u_name, u_username, u_email, u_phone, u_website) {
+    console.log(u_id, u_name, u_username, u_email, u_phone, u_website);
+    //logic for popup
+    temp2 ="";
+    temp2 += "<ul>";
+    temp2 += "<li> Name :"+ "<span>"+u_name+"</span>"+"</li>"
+    temp2 += "<li> Username :"+ "<span>"+u_username+"</span>"+"</li>"
+    temp2 += "<li> Email :"+ "<span>"+u_email+"</span>"+"</li>"
+    temp2 += "<li> Phone :"+ "<span>"+u_phone+"</span>"+"</li>"
+    temp2 += "<li> Website :"+ "<span>"+u_website+"</span>"+"</li>"
+    document.querySelector(".content__data").innerHTML = temp2;
+    
+    togglePopup()
+
+
+
+}
+function togglePopup() {
+    document.getElementById("popup1").classList.toggle("active");
 }
 
 //invoking getData Function 
