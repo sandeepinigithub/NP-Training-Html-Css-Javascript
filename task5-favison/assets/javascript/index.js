62057b5b342b759b52fd2720d97e23bc1a5c7571
Sandeep Shukla <sandeepini.2012@gmail.com>
2021-05-19 00:34:23 +0530

    task5-favison-header partial1.0

---
+++ task5-favison/assets/javascript/index.js
@@ -0,0 +1,49 @@
+const toggle = document.querySelector(".navbar-toggler");
+const menu = document.querySelector(".menu");
+const items = document.querySelectorAll(".item");
+/* Toggle mobile menu */
+function toggleMenu() {
+    if (menu.classList.contains("active")) {
+        menu.classList.remove("active");
+        // adds the menu (hamburger) icon
+        if (toggle.getAttribute("aria-expanded")) {
+            toggle.setAttribute("aria-expanded", "false");
+        }
+    } else {
+        menu.classList.add("active");
+        // adds the close (x) icon
+        if (toggle.getAttribute("aria-expanded")) {
+            toggle.setAttribute("aria-expanded", "true");
+        }
+    }
+}
+/* Event Listener */
+toggle.addEventListener("click", toggleMenu, false);
+/* Activate Submenu */
+function toggleItem() {
+    if (this.classList.contains("submenu-active")) {
+        this.classList.remove("submenu-active");
+    } else if (menu.querySelector(".submenu-active")) {
+        menu.querySelector(".submenu-active").classList.remove("submenu-active");
+        this.classList.add("submenu-active");
+    } else {
+        this.classList.add("submenu-active");
+    }
+}
+/* Event Listeners */
+for (let item of items) {
+    if (item.querySelector(".submenu")) {
+        item.addEventListener("click", toggleItem, false);
+        item.addEventListener("keypress", toggleItem, false);
+    }
+}
+/* Close Submenu From Anywhere */
+function closeSubmenu(e) {
+    let isClickInside = menu.contains(e.target);
+
+    if (!isClickInside && menu.querySelector(".submenu-active")) {
+        menu.querySelector(".submenu-active").classList.remove("submenu-active");
+    }
+}
+/* Event listener */
+document.addEventListener("click", closeSubmenu, false);
